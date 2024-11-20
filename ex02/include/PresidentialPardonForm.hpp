890da1f12eece2a7bff92cc803050830e046c02e
fgori42 <98gori@gmail.com>
2024-11-20 14:57:34 +0100

    ll

--- ex02/include/PresidentialPardonForm.hpp
+++ ex02/include/PresidentialPardonForm.hpp
@@ -6,24 +6,31 @@
 class PresidentialPardonForm : public AForm
 {
 private:
-	std::string	_name;
+	std::string	_targhet;
 public:
 	PresidentialPardonForm();
-	//PresidentialPardonForm(std::string name);
-	//PresidentialPardonForm(const PresidentialPardonForm &org);
+	PresidentialPardonForm(std::string name);
+	PresidentialPardonForm(const PresidentialPardonForm &org);
 	~PresidentialPardonForm();
 
 	PresidentialPardonForm &operator=(const PresidentialPardonForm &org);
 };
 
-PresidentialPardonForm::PresidentialPardonForm(): AForm("default", 145, 137)
+PresidentialPardonForm::PresidentialPardonForm(): AForm("default", 25, 5)
 {
 	std::cout << "PresidentialPardonForm default creator" << std::endl;
-	_name = "default";
+	_targhet = "default";
+}
+
+PresidentialPardonForm::PresidentialPardonForm(std::string target): AForm("default", 25, 5)
+{
+	_targhet = target;
+	std::cout << "PresidentialPardonForm creator target: " << _targhet << std::endl;
 }
 
 PresidentialPardonForm::~PresidentialPardonForm()
 {
+	std::cout << "PresidentialPardonForm destructor target: " << _targhet << std::endl;
 }
 
 #endif
